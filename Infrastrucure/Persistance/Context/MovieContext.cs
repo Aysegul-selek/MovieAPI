b6c44d0ba25d0f1ba156de44f6e26b59552068c3
Aysegul-selek <aysegul.selek03@gmail.com>
2025-03-04 16:25:42 +0300

    Database created

---
+++ Infrastrucure/Persistance/Context/MovieContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieApi.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistance.Context
+{
+    public class MovieContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=MovieDb;Trusted_Connection=True;");
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Review> Reviws { get; set; }
+        public DbSet<Cast> Casts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
