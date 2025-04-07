c5cd202004c83dc06f81b9551c4dea68dbb9a9b7
Aysegul-selek <aysegul.selek03@gmail.com>
2025-04-07 16:51:03 +0300

    Cast.controller added.

---
+++ MovieAPI.WebAPI/Controllers/CastsController.cs
@@ -0,0 +1,51 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MovieAPI.Application.Features.MediatorDesignPattern.Commands.CastCommands;
+using MovieAPI.Application.Features.MediatorDesignPattern.Queries;
+
+namespace MovieAPI.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CastsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public CastsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpGet]
+        public IActionResult CastList()
+        {
+            var value = _mediator.Send(new GetCastQuery());
+            return Ok(value);
+        }
+        [HttpPost]
+        public IActionResult CreateCast(CreateCastCommand command)
+        {
+            _mediator.Send(command);
+            return Ok("ekleme başarılı");
+        }
+        [HttpDelete]
+        public IActionResult DeleteCast(int id)
+        {
+            _mediator.Send(new RemoveCastCommand(id));
+            return Ok("silme başarılı");
+        }
+        [HttpPut]
+        public IActionResult UpdateCast(UpdateCastCommand command)
+        {
+            _mediator.Send(command);
+            return Ok("güncelleme başarılı");
+            
+        }
+        [HttpGet("GetCastById")]
+        public IActionResult GetCastById(int id)
+        {
+            var value = _mediator.Send(new GetCastByIdQuery(id));
+            return Ok(value);
+        }
+    }
+}
