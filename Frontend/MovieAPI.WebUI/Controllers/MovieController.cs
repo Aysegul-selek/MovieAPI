b4fba50e5eaa467a727e0fbec6f89de6b17debb3
Aysegul-selek <aysegul.selek03@gmail.com>
2025-04-14 00:28:52 +0300

    UILayout devamı kodlandı footera kadar

---
+++ Frontend/MovieAPI.WebUI/Controllers/MovieController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieAPI.WebUI.Controllers
+{
+    public class MovieController : Controller
+    {
+        public IActionResult MovieList()
+        {
+            ViewBag.v1 = "Film Listesi";
+            ViewBag.v1 = "Ana Sayfa";
+            ViewBag.v1 = "Tüm Filmler";
+            return View();
+        }
+    }
+}
